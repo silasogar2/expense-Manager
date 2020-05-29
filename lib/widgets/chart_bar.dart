3b4480566c0cda4936fe4c2bb1e819284837ce5a
jendyjasper <silasogar2@gmail.com>
2020-05-29 16:42:14 +0100

    Updated

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+ final String label;
+ final double spendingAmount;
+ final double finalPercentageOfTotal;
+
+ ChartBar(this.label, this.spendingAmount, this.finalPercentageOfTotal)
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(height: 4.0,),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                color: Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ],),
+        ),
+        SizedBox(height: 4,),
+        Text(label),
+      ],
+    );
+  }
+}
